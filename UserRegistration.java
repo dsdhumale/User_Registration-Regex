3d78be127e55cc11439e9733aa46ececc3781df1
dsdhumale <d.sdhumale@gmail.com>
2022-11-01 18:00:06 +0530

    Added : Ability to check mobile number is valid or not

--- UserRegistration.java
+++ UserRegistration.java
@@ -18,6 +18,11 @@ public class UserRegistration {
 		return email.matches("^[a-z]{3,}[.+-_]*[a-z0-9]*{3,}[@][a-z0-9]{1,}[.][a-z]{2,}[.]*[a-z]*{2,}$");
 	}
 
+	// Method for validation of Mobile number -- UC4
+	public static boolean mobileNumber(String mobilenum) {
+		return mobilenum.matches("^[0-9]{2}[\s][0-9]{10}$");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome in User registration program using Regex");
 		Scanner input = new Scanner(System.in);
@@ -41,6 +46,14 @@ public class UserRegistration {
 			System.out.println(email + " : is a valid email id");
 		} else {
 			System.out.println(email + " : is a invalid email id");
+		}
+		System.out.println("Enter mobile number");
+		input = new Scanner(System.in);
+		String mobilenum = input.nextLine();
+		if (mobileNumber(mobilenum)) {
+			System.out.println(mobilenum + " : is a valid mobile number");
+		} else {
+			System.out.println(mobilenum + " : is a invalid mobile number");
 		}
 	}
 }
