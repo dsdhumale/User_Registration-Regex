6c004d72bc544ee4d1c0ef9f70de7cc04fc5ad23
dsdhumale <d.sdhumale@gmail.com>
2022-11-01 17:43:03 +0530

    Added : Welcome message

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz.Day17;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome in User registration program using Regex");
+	}
+}
